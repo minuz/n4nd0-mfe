967641b364d647c9b21ffdd15f38c5b0650e587f
Fernando Baba <f.baba@nibtravel.com>
2021-10-03 13:18:54 +1100

    initial commit

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'n4nd0-mfe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'playground';
+}
